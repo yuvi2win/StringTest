d6d9f79983eec64ba417c3157aa7709ca5ed0288
yuvi2win <86060292+yuvi2win@users.noreply.github.com>
2021-07-16 19:35:02 +0530

    Add files via upload

---
+++ StringSplit.java
@@ -0,0 +1,26 @@
+package javaPrograms;
+
+class StringSplit {
+	static void splitString(String str) {
+		StringBuffer alpha = new StringBuffer(), num = new StringBuffer(), special = new StringBuffer();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i)))
+				num.append(str.charAt(i));
+			else if (Character.isAlphabetic(str.charAt(i)))
+				alpha.append(str.charAt(i));
+			else
+				special.append(str.charAt(i));
+		}
+
+		System.out.println(alpha);
+		System.out.println(num);
+		System.out.println(special);
+	}
+
+	// Driver method
+	public static void main(String args[]) {
+		String str = "geeks01$$for02geeks03!@!!434gfdjfgy23yfe49324923ufhreir394494";
+		splitString(str);
+	}
+}
